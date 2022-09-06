dd4da828f5e6bb9fd8bba80831ede1361161e808
Chan Jessica <e.jchan@etu.cvm.qc.ca>
2022-09-06 12:42:28 -0400

    code répétitif à raffiner

--- C62/TP1/Recherche.py
+++ C62/TP1/Recherche.py
@@ -1,45 +1,67 @@
+from time import perf_counter
+import numpy as np
+
 class Recherche:
     def __init__(self, entrainement, data):
         self.matrice = entrainement.m
         self.dict_mots = entrainement.dict_mots
-        self.dict_score = dict()
-        self.mot_recherche = data[0]
+        self.scores = []
+        self.idx_mot_recherche = self.dict_mots[data[0]]
         self.limite = data[1]
+        self.option = data[2]
         self.decroissant = True
     
     def produit_scalaire(self):
-        idx_mot_recherche = self.dict_mots[self.mot_recherche]
-        for idx, rangee in enumerate(self.matrice):
-            score = sum(self.matrice[idx_mot_recherche] * self.matrice[idx])
-            mot_compare = self.chercher_cle(self.dict_mots, idx)
-            self.dict_score[mot_compare] = score
+        for mot_compare, idx in self.dict_mots.items():
+            score = np.sum(self.matrice[self.idx_mot_recherche] * self.matrice[idx])
+            self.scores.append((score, mot_compare))
+        # self.calcul_score()
     
     def least_squares(self):
         self.decroissant = False
-        print("ls")
-    
+        for mot_compare, idx in self.dict_mots.items():
+            score = np.sum(np.power(self.matrice[self.idx_mot_recherche] - self.matrice[idx], 2))
+            self.scores.append((score, mot_compare))
+        # self.calcul_score()
+        
     def city_block(self):
         self.decroissant = False
-
-        print("cb")
-    
+        for mot_compare, idx in self.dict_mots.items():
+            score = np.sum(np.abs(self.matrice[self.idx_mot_recherche] - self.matrice[idx]))
+            self.scores.append((score, mot_compare))
+        # self.calcul_score()
+            
+    # def calcul_score(self):
+        
+    #     for mot_compare, idx in self.dict_mots.items():
+    #         match self.option:
+    #             case 0:
+    #                 score = np.sum(self.matrice[self.idx_mot_recherche] * self.matrice[idx])
+    #             case 1:
+    #                 score = np.sum(np.power(self.matrice[self.idx_mot_recherche] - self.matrice[idx], 2))
+    #             case 2:
+    #                 score = np.sum(np.abs(self.matrice[self.idx_mot_recherche] - self.matrice[idx]))
+        
+    #         self.scores.append((score, mot_compare))
+        
     def chercher_cle(self, dictionnaire, v):
         for cle, valeur in dictionnaire.items():
             if v == valeur:
                 return cle
 
     def afficher_resultat(self):
-        scores_croissant = sorted(self.dict_score.values(), reverse=self.decroissant)
+        scores_croissant = sorted(self.scores, reverse=self.decroissant)
         resultats = []
         counter = 0
-        for score in scores_croissant:
-                cle = self.chercher_cle(self.dict_score, score)
-                resultats.append(str(cle) + " --> " + str(score))
-            
-        for resultat in resultats:
+        for score, mot in scores_croissant:
             if counter < int(self.limite):
-                print(resultat)
-            counter += 1
+                resultats.append(str(mot) + " --> " + str(score))
+                counter += 1
+            else:
+                break
+           
+        for resultat in resultats:
+            print(resultat)
                 
             
             
--- C62/TP1/main.py
+++ C62/TP1/main.py
@@ -6,6 +6,7 @@ import re
 
 def interface_usager(fenetre, entrainement):
     rep = ""
+    
     while rep != 'q':
     
         rep = input('''
@@ -15,37 +16,41 @@ i.e. produit scalaire: 0, least-squares: 1, city-block: 2
 Tapez q pour quitter 
 
 ''')
+        if rep == 'q':
+            return
+        
         reponse = re.findall('\w+', rep)
         recherche = Recherche(entrainement, reponse)
-        
+
+        start_counter = perf_counter()
         match reponse[2]:
             case '0':
                 recherche.produit_scalaire()
+                end_counter = perf_counter()
             case '1':
                 recherche.least_squares()
+                end_counter = perf_counter()
             case '2':
                 recherche.city_block()
+                end_counter = perf_counter()
             case default:
                 return             
     
+        start_counter_aff = perf_counter()
         recherche.afficher_resultat()
-
-    return
+        end_counter_aff = perf_counter()
+        print(f"time fonction : {end_counter-start_counter}")
+        print(f"time aff : {end_counter_aff-start_counter_aff}")
 
 def main():
-    # fenetre, encodage, path = argv[1:]
-    fenetre = 5
-    encodage = 'utf-8'
-    path = 'C:\\Users\\1830222\\Documents\\ChanJessica_PerezSebastian\\C62\\TP1\\textes\\test.txt'
-    
+    fenetre, encodage, path = argv[1:]
+    # fenetre = 5
+    # encodage = 'utf-8'
+    # path = 'C:\\Users\\1830222\\Documents\\ChanJessica_PerezSebastian\\C62\\TP1\\textes\\test.txt'
     
     entrainement = Entrainement()
-    start_counter = perf_counter()
     entrainement.creation_matrice(int(fenetre), path, encodage)
-    end_counter = perf_counter()
-    
-    print(f"time : {end_counter-start_counter}")
-   
+
     rep = interface_usager(fenetre, entrainement)
     if rep == 'q':
         quit()
