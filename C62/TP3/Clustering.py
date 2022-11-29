d293c448c973dfb048be430f1040fd1e014c2655
jessicach4n <e.jchan@etu.cvm.qc.ca>
2022-11-29 10:51:56 -0500

    calcul

--- C62/TP3/Clustering.py
+++ C62/TP3/Clustering.py
@@ -21,9 +21,6 @@ class Clustering:
         # self.__nouveux_centroides = np.zeros((len(self.__dict_mots), nbre_centroides), dtype=float)
         # self.__matrice_asso_mot_cluster = np.zeros((len(self.__dict_mots),2))
 
-        # for id_mot, id_occurence, nb_occurence in self.__liste_synonymes:
-        #     self.__matrice[id_mot,id_occurence] = nb_occurence
-
         self.__creer_matrice()
         self.__creer_centroides_depart()
         self.__calculer_cluster()
@@ -33,24 +30,26 @@ class Clustering:
         counter = 0
         nb_iteration = 0
         max_iter = 3 # Après aucun changement
-        aucun_changement = np.array_equal(self.__ancien_cluster, self.__cluster)
-        while aucun_changement and counter < max_iter: 
+        nb_changement = len(self.__matrice)
+        # aucun_changement = np.array_equal(self.__ancien_cluster, self.__cluster)
+        # while aucun_changement and counter < max_iter: 
+        while nb_changement > 0:
             temps_depart = perf_counter()
-            self.__recalculer_centroide()
-            self.__calculer_cluster()
-            print(f'\n iteration no : {nb_iteration}')
-            if aucun_changement: 
-                counter += 1 
+            population_cluster = self.__recalculer_centroide()
+            nb_changement = self.__calculer_cluster()
+            # if aucun_changement: 
+                # counter += 1 
+            
             nb_iteration += 1
             temps_fin = perf_counter()
             
-            population_cluster = []
-            for centroide in range(self.__nbre_centroides):
-                population_cluster.append(np.count_nonzero(self.__cluster == centroide))
-                print(f'self centroides {self.__centroides}')
-                print( f'popuplation cluster :  {population_cluster}')
-                print( f'centroide  :  {centroide}')
-            nb_changement = 2
+            # population_cluster = []
+            # for centroide in range(self.__nbre_centroides):
+            #     population_cluster.append(np.count_nonzero(self.__cluster == centroide))
+            #     # print(f'self centroides {self.__centroides}')
+                # print( f'popuplation cluster :  {population_cluster}')
+                # print( f'centroide  :  {centroide}')
+            # nb_changement = '#'
             self.__print_message(nb_iteration, temps_fin - temps_depart, nb_changement, population_cluster)
 
 
@@ -60,7 +59,6 @@ class Clustering:
         for idx, cluster in enumerate(population_cluster):
             message += f"\nIl y a {cluster} mots appartenant au centroide {idx}"
             
-        
         print(separateur)
         print(message)
         print(separateur)
@@ -74,13 +72,14 @@ class Clustering:
                 random_values.append(idx_mot)
                 i += 1
         for idx in range(self.__nbre_centroides) :
-            self.__centroides [idx]= self.__matrice[random_values[idx]]
+            self.__centroides[idx] = self.__matrice[random_values[idx]]
 
-        print('position centroides depart \n')
-        print(self.__centroides)
+        # print('position centroides depart \n')
+        # print(self.__centroides)
 
     def __calculer_cluster(self) -> None:
         self.__cluster  = np.arange(len(self.__matrice))
+        # print(f'self.__cluster : {self.__cluster}')
         for i in range(len(self.__dict_mots)):
             d = [] 
             for c in self.__centroides:
@@ -89,9 +88,12 @@ class Clustering:
             centroide = d.index(min(d))
             self.__cluster[i] = centroide
 
+        # print(f'self.__cluster 2 : {self.__cluster}')
+        nb_changement = np.sum(np.not_equal(self.__ancien_cluster, self.__cluster))
         self.__ancien_cluster = self.__cluster
-        print(" chaque mots appartient à quel cluster \n")
-        print(self.__cluster)
+        # print(" chaque mots appartient à quel cluster \n")
+        # print(self.__centroides)
+        return nb_changement
 
     def __recalculer_centroide(self):
         self.__centroides = np.zeros((self.__nbre_centroides,len(self.__dict_mots) ), dtype=float)
@@ -105,9 +107,10 @@ class Clustering:
             if decompte[i] > 0 :
                 self.__centroides[i] /= decompte[i]
 
-        print('position de nouveaux centroide \n')
-        print(self.__centroides)
-        
+        return decompte
+
+        # print('position de nouveaux centroide \n')
+        # print(self.__centroides)
 
     def __creer_dictionnaire_mots(self, dao:Dao) -> None:
         for id, mot in dao.select_from_dictionnaire():
